a2cba3bd21a9a1b0ac9921acef0bcb16aa18ee8c
Jamie Barton <jamie@launchmade.com>
2017-07-28 20:26:43 +0100

    Tutorial code

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const { makeExecutableSchema } = require('graphql-tools')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const products = [
+	{ id: 1, name: 'NOR810', sku: 'nor810' },
+	{ id: 2, name: 'NOR820', sku: 'nor820' }
+]
+
+const typeDefs = `
+  type Product {
+    id: Int!
+    name: String
+    sku: String
+  }
+
+  type Query {
+    allProducts: [Product]
+  }
+`
+
+const resolvers = {
+	Query: {
+		allProducts: () => products
+	}
+}
+
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers
+})
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+
+app.use(
+	'/graphiql',
+	graphiqlExpress({
+		endpointURL: '/graphql'
+	})
+)
+
+app.listen(4000)
